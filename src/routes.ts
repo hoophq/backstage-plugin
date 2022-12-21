48880d132dfaab8a1bb01bf35debc2b247da509a
Rogerio Moura <roger0.rm@gmail.com>
2022-12-21 11:43:24 -0300

    fix(routes): remove unnecessary code

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,3 @@ import { createRouteRef } from '@backstage/core-plugin-api';
 export const rootRouteRef = createRouteRef({
   id: 'hoop',
 });
-
-export const entityContentRouteRef = createRouteRef({
-  id: 'lighthouse:entity-content',
-});
